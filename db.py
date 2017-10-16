259d16ef92f399ed7d589fce780bb616d83767f0
Larry Wainwright <lwainwright1124@gmail.com>
2017-10-16 14:24:15 -0600

    Added method comments to try to explain what things do

--- db.py
+++ db.py
@@ -3,8 +3,17 @@ from mysql.connector import errorcode
 
 
 class DB:
+    """
+    Interface to ease querying against mysql
+    """
 
     def __init__(self, database, user, pwd):
+        """
+        Constructor - stores the connection info and tries to create a connection.
+        :param database:
+        :param user:
+        :param pwd:
+        """
         self.database = database
         self.user = user
         self.pwd = pwd
@@ -18,12 +27,24 @@ class DB:
             pass
 
     def __del__(self):
+        """
+        Destructor, destroy the connection
+        :return: None
+        """
         try:
             self.conn.close()
         except:
             pass
 
     def connect(self, database, user, pwd, host='localhost'):
+        """
+        Connects to mysql and the the referenced database.
+        :param database: name of database to connect to
+        :param user: user to log in as
+        :param pwd: password of user
+        :param host: host name - default is localhost
+        :return: None
+        """
         self.database = database
         self.user = user
         self.pwd = pwd
@@ -39,6 +60,11 @@ class DB:
                 print err
 
     def select_database(self, db_name):
+        """
+        switches to different database.
+        :param db_name: name of database to switch to
+        :return: None
+        """
         try:
             self.conn.database = db_name
             self.database = db_name
@@ -46,6 +72,12 @@ class DB:
             print err.msg
 
     def get_cursor(self):
+        """
+        Retrieves a cursor from the connection.  By default it causes the cursor to return data as a
+        dictionary.  This is not always desirable, for instance, when using a stored procedure.  In those
+        cases, it may be more desirable to get the cursor yourself or use the proc method.
+        :return: cursor
+        """
         cursor = None
         try:
             cursor = self.conn.cursor(dictionary=True)
@@ -55,6 +87,13 @@ class DB:
         return cursor
 
     def execute(self, sql, params=None, commit=False):
+        """
+        Executes a sql statement
+        :param sql: sql statement
+        :param params: params in the query in the form of a dictionary
+        :param commit: True/False, whether to commit
+        :return: None or auto incremented id if an insert statement
+        """
         id = None
         cursor = self.get_cursor()
         if not cursor:
@@ -77,6 +116,13 @@ class DB:
         return id
 
     def execute_ret_cursor(self, sql, params=None):
+        """
+        Intended to be used internally by this class.  Creates a cursor, executes the query
+        and returns the cursor for further operations, such as retrieving results.
+        :param sql: sql query
+        :param params: params in dictionary form
+        :return: cursor
+        """
         cursor = self.get_cursor()
         if not cursor:
             return None
@@ -93,6 +139,12 @@ class DB:
         return cursor
 
     def fetch_one(self, sql, params=None):
+        """
+        Returns one record result from the query
+        :param sql: sql query statement
+        :param params: dictionary of params
+        :return: dictionary
+        """
         cursor = self.execute_ret_cursor(sql, params)
         row = {}
         if cursor:
@@ -106,6 +158,12 @@ class DB:
         return row
 
     def fetch_all(self, sql, params=None):
+        """
+        Retrieves all records of the result of the query
+        :param sql: sql query statement
+        :param params: dictionary of params
+        :return: array of dictionary based records.
+        """
         cursor = self.execute_ret_cursor(sql, params)
         data = []
         if cursor:
@@ -116,6 +174,13 @@ class DB:
         return data
 
     def fetch_col(self, col_name, sql, params=None):
+        """
+        Retrieves a single column from the first record of the result set.
+        :param col_name: column name to retrieve data for
+        :param sql: sql query
+        :param params: dictionary of params
+        :return: value of the named column requested in col_name.
+        """
         cursor = self.execute_ret_cursor(sql, params)
         result = ''
         if cursor:
