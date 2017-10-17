3f885b0a96fdda5489ddbe68bebe9299a9809e55
Larry Wainwright <lwainwright1124@gmail.com>
2017-10-17 17:03:09 -0600

    Added more content to stuff.py as example of working with various things, mostly arrays and dictionaries.  Small things that are missed when first learning the language.

--- stuff.py
+++ stuff.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 
 class stuff:
     def __init__(self):
@@ -11,4 +12,56 @@ obj = stuff()
 print "this is reference to objects x value: ", obj.x
 print "now we call the objects runme method which prints self.x: ",
 obj.runme()
+
+print '\nlist all properties of an object:'
+print dir(obj)
+print '\nnow list all properties of an object that arent built in'
+print 'also is example of a list comprehension that is too complicated for you to look at right now'
+print 'but, good to keep in mind for later'
+print [x for x in dir(obj) if x[:2] != '__' and x[:-2] != '__']
+raw_input('press enter to continue')
+
+# arrays, they can hold anything and different data types in them if necessary but that isn't very common
+arr = ['this', 'is', 'an', 'array', 10, 20]
+print '\nprinting array: ', arr
+# loop thru array
+for el in arr:
+    print el
+raw_input('press enter to continue')
+print '-' * 80
+
+# loop thru array and get index at same time also
+# introduction to string format function
+print '\nloop array getting both value and its index using enumerate function'
+for idx, el in enumerate(arr):
+    print 'Array element {idx} contains value: {value}'.format(idx=idx, value=el)
+raw_input('press enter to continue')
+print '-' * 80
+
+
+# date creation, date math and dictionary
+print '\ndate creation, date math and dictionary'
+mydate = datetime.now() - timedelta(days=4)
+print 'today - 4 is: ', mydate
+raw_input('press enter to continue')
+
+d = {'name': 'shithead', 'value': 'asshole', 'dt': mydate}
+print '\ndump the whole dictionary out:'
+print d
+print 'printing a value for name from the dictionary: ', d['name']
+print 'iterate over a dictionary:'
+for key in d:
+    print key
+
+print '\nnotice, no values? that requires one of 2 techniques:'
+raw_input('press enter to continue')
+for key in d:
+    print 'key: ', key, 'value: ', d[key]
+
+raw_input('press enter to continue')
+
+print '\nother technique (and more commonly used):'
+for key, value in d.iteritems():
+    print 'key: ', key, 'value: ', value
+
 
