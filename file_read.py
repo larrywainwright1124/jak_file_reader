9f9d364525d0fd68648b59d6758e131069ae9ed2
Jakson Wainwright <jaxonawain@gmail.com>
2017-10-18 14:00:47 -1000

    Update file_read.py

--- file_read.py
+++ file_read.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-from decimal import Decimal
-
 """
 What I want to do is create a class that is to read the whole document
 But I think that means that I need to create a class, and then subclasses underneath that do the following:
@@ -14,41 +11,52 @@ But I think that means that I need to create a class, and then subclasses undern
 I dont think the following is the right way to put the information into a dictionary ? I don't really know how to output the key - value pairs in the dictionary "headerinfo"
 I also dont know
 """
+"""
+having difficulties understanding the parameters I need to put in
+"""
 
 """
 headerInfo = [{'tRecs': tRecs, 'tDebit': tDebit, 'tDebitAmt': tDebitAmt, 'tPay': tPay}]
 """
 
-f = open("thefile.txt", "r")
-
-"""
-having difficulties understanding the parameters I need to put in
-"""
+from datetime import datetime
+from decimal import Decimal
 
 
-def readfile(self, f):
-    f = open('thefile.txt', "r")
+class Readheader:
+    def __init__(self,):
+        i = 0
+        f = open('thefile.txt', "r")
+        while i < self.tRec * 9:
+            self.tRecsStr = f.read(6)
+            self.tRec = int(self.tRecsStr)
+            self.tDebitStr = f.read(6)
+            self.tPayStr = f.read(6)
+            self.tDebitAmtStr = f.read(12)
+            self.tDebitAmt = float(int(self.tDebitAmtStr) * .01)
+            self.tPaysAmtStr = f.read(12)
+            self.tPayAmt = float(int(self.tPaysAmtStr) * .01)
 
-    headerTrecs = int(self.tRecs)
+            self.index = f.read(6)
+            self.tType = f.read(3)
+            self.accNum = f.read(16)
+            self.dateRaw = f.read(8)
+            self.date = datetime(year=int(self.dateRaw[0:4]), month=int(self.dateRaw[4:6]), day=int(self.dateRaw[6:8]))
+            self.name = f.read(25)
+            self.city = f.read(23)
+            self.state = f.read(2)
+            self.acqCode = f.read(5)
+            self.tAmountRaw = f.read(12)
+            self.tAmount = int(self.tAmountRaw) * .01
+            self.n = '\n'
 
-    self.tRecs = f.read(6)
-    self.tDebit = f.read(6)
-    self.tPay = f.read(6)
-    self.tDebitAmtRaw = f.read(12)
-    self.tDebitAmt = float(int(self.tDebitAmtRaw) * .01)
-    self.tPaysAmtRaw = f.read(12)
-    self.tPayAmt = float(int(self.tPaysAmtRaw) * .01)
+            record = [self.index, self.name, self.tType, self.accNum, self.date, self.city, self.state, self.acqCode, self.tAmount, self.n]
+            for item in record:
+                print (item)
+                i = i + 1
 
-    self.index = f.read(6)
-    self.tType = f.read(3)
-    self.accNum = f.read(16)
-    self.dateRaw = f.read(8)
-    self.date = datetime(year=int(self.dateRaw[0:4]), month=int(self.dateRaw[4:6]), day=int(self.dateRaw[6:8]))
-    self.name = f.read(25)
-    self.city = f.read(23)
-    self.state = f.read(2)
-    self.acqCode = f.read(5)
-    self.tAmountRaw = f.read(12)
-    self.tAmount = int(self.tAmountRaw) * .01
+
+
+
 
 
